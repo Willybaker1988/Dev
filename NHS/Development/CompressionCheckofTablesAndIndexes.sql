16302a0cadd8f316d2038f3ce96df8c5c4d8b588
William Baker <willbaker1988@hotmail.co.uk>
2017-12-19 12:46:49 +0000

    Added Indexes to datawarehouse table and added page compression to optimise on saving space. Also created the TableIndexes script for deployement, yet to be checked but is a starting point for deployement

--- NHS/Development/CompressionCheckofTablesAndIndexes.sql
+++ NHS/Development/CompressionCheckofTablesAndIndexes.sql
@@ -2,7 +2,10 @@
 The type column indicates the type of the index. 
 1 is for clustered indexes. 2 means a non-clustered index. 0 (not shown in this example) is a heap.
  Recall that a table is either a heap or a clustered index.
+ https://www.sqlshack.com/use-sql-server-data-compression-save-space/
 */
+use NHS;
+go
 
 SELECT DISTINCT
     s.name,
@@ -22,4 +25,29 @@ ON i.index_id = p.index_id
     AND t.object_id = p.object_id
 WHERE t.type = 'U' 
   AND p.data_compression_desc = 'NONE'
-ORDER BY p.rows desc
+ORDER BY p.rows desc 
+
+SELECT name,
+    s.used / 128.0                  AS SpaceUsedInMB,
+    size / 128.0 - s.used / 128.0   AS AvailableSpaceInMB
+FROM sys.database_files
+CROSS APPLY 
+    (SELECT CAST(FILEPROPERTY(name, 'SpaceUsed') AS INT)) 
+s(used)
+WHERE FILEPROPERTY(name, 'SpaceUsed') IS NOT NULL;
+
+
+
+EXEC sp_estimate_data_compression_savings 
+    @schema_name = 'Datawarehouse', 
+    @object_name = 'FactPrescription', 
+    @index_id = NULL, 
+    @partition_number = NULL, 
+    @data_compression = 'ROW'
+ 
+EXEC sp_estimate_data_compression_savings 
+    @schema_name = 'Datawarehouse', 
+    @object_name = 'FactPrescription', 
+    @index_id = NULL, 
+    @partition_number = NULL, 
+    @data_compression = 'PAGE'
