b2cd15638bae9545d9d5bf07417d1f36f7f3d6ac
William Baker <willbaker1988@hotmail.co.uk>
2018-01-21 15:51:41 +0000

    added powershell script to PrescriptionDataLoad to deal with filesnames with spaces. Also added 2 new indexes to Stage.gpaddress and Transform.DimGeneralPracticeAddress

--- NHS/Scripts/CreateIndexes.sql
+++ NHS/Scripts/CreateIndexes.sql
@@ -2,25 +2,37 @@
 
 USE NHS;
 
+
+IF NOT EXISTS (SELECT [Name] FROM sys.sysindexes WHERE [Name] = 'iX_Stage_GPAddress_FileLogId')
+	CREATE CLUSTERED INDEX iX_Stage_GPAddress_FileLogId on [Stage].[GPAddress] ([FileLogId])
+	PRINT '...Created CLUSTERED INDEX iX_Stage_GPAddress_FileLogId'
+
 IF NOT EXISTS (SELECT [Name] FROM sys.sysindexes WHERE [Name] = 'iX_Stage_Prescription_Period_Product_Practice')
-	CREATE CLUSTERED INDEX iX_Stage_Prescription_Period_Product_Practice on [NHS].[Stage].[Prescription] ([PERIOD], [BNFCode], [PRACTICE])
+	CREATE CLUSTERED INDEX iX_Stage_Prescription_Period_Product_Practice on [Stage].[Prescription] ([PERIOD], [BNFCode], [PRACTICE])
 	PRINT '...Created CLUSTERED INDEX iX_Stage_Prescription_Period_Product_Practice'
 
 IF NOT EXISTS (SELECT [Name] FROM sys.sysindexes WHERE [Name] = 'iX_Stage_ChemicalSubstance_Period_ChemSubId')
-	CREATE CLUSTERED INDEX iX_Stage_ChemicalSubstance_Period_ChemSubId on [NHS].[Stage].[ChemicalSubstance] ([PERIOD], CHEMSUBId)
+	CREATE CLUSTERED INDEX iX_Stage_ChemicalSubstance_Period_ChemSubId on [Stage].[ChemicalSubstance] ([PERIOD], CHEMSUBId)
 	PRINT '...Created CLUSTERED INDEX iX_Stage_ChemicalSubstance_Period_ChemSubId'
 
 IF NOT EXISTS (SELECT [Name] FROM sys.sysindexes WHERE [Name] = 'iX_Mirror_FactPrescription_PeriodId')
-	CREATE CLUSTERED INDEX iX_Mirror_FactPrescription_PeriodId on [NHS].[Stage].[ChemicalSubstance] ([PERIOD], CHEMSUBId)
+	CREATE CLUSTERED INDEX iX_Mirror_FactPrescription_PeriodId on [Mirror].[FactPrescription] ([PERIOD], CHEMSUBId)
 	PRINT '...Created CLUSTERED INDEX iX_Mirror_FactPrescription_PeriodId'
 
 IF NOT EXISTS (SELECT [Name] FROM sys.sysindexes WHERE [Name] = 'iX_Mirror_DimGeneralPracticeAddress')
-	CREATE CLUSTERED INDEX iX_Mirror_FactPrescription_PeriodId on [NHS].[Stage].[ChemicalSubstance] ([PERIOD], CHEMSUBId)
+	CREATE CLUSTERED INDEX iX_Mirror_FactPrescription_PeriodId on [Stage].[ChemicalSubstance] ([PERIOD], CHEMSUBId)
 	PRINT '...Created CLUSTERED INDEX iX_Mirror_DimGeneralPracticeAddress'
 
 IF NOT EXISTS (SELECT [Name] FROM sys.sysindexes WHERE [Name] = 'iX_Mirror_DimProduct_Period')
-	CREATE CLUSTERED INDEX iX_Mirror_DimProduct_Period on [NHS].[Mirror].[DimProduct] (BNFCODE)
+	CREATE CLUSTERED INDEX iX_Mirror_DimProduct_Period on [Mirror].[DimProduct] (BNFCODE)
 	PRINT '...Created CLUSTERED INDEX iX_Mirror_DimProduct_Period'
+
+IF NOT EXISTS (SELECT [Name] FROM sys.sysindexes WHERE [Name] = 'iX_Transform_DimGeneralPracticeAddress_FileLogId')
+	CREATE CLUSTERED INDEX iX_Transform_DimGeneralPracticeAddress_FileLogId on [Transform].[DimGeneralPracticeAddress] ([FileLogId])
+	PRINT '...Created CLUSTERED INDEX iX_Transform_DimGeneralPracticeAddress_FileLogId'
+
+
+
 
 
 
